66f60dabff9e5e7285396a3fb2fbe69d867acc27
Pau Gargallo <pau.gargallo@gmail.com>
2010-11-21 14:02:05 +0100

    Add the settings view controller.

---
+++ src/iphone/Classes/SettingsViewController.h
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.h
+//  StereoPlanner
+//
+//  Created by Pau Gargallo on 11/21/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SettingsViewController : UITableViewController {
+
+}
+
+@end
